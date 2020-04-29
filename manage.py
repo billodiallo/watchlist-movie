94a9126516bfe4fb16907009f52dd05fedba31db
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-29 15:00:00 +0300

    import db

--- manage.py
+++ manage.py
@@ -1,17 +1,20 @@
 from app import create_app
 from flask_script import Manager, Server
-#from flask_migrate import Migrate, MigrateCommand
-#from app.models import User,Role,Review
+from flask_migrate import Migrate, MigrateCommand
+from app.models import User
+#Role,
+#Review
 #from werkzeug import secure_filename
 #from werkzeug.utils import secure_filename
+from app import create_app,db
 # Creating the app instance
 app = create_app('development')
 
 
-#migrate = Migrate(app,db)
+migrate = Migrate(app,db)
 manager = Manager(app)
 manager.add_command('server',Server)
-#manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
@@ -22,9 +25,9 @@ def test():
 	tests = unittest.TestLoader().discover('tests')
 	unittest.TextTestRunner(verbosity=2).run(tests)
 
-#@manager.shell
-#def make_shell_context():
-#	return dict(app=app, db=db, User=User, Role=Role, Review=Review)
+@manager.shell
+def make_shell_context():
+	return dict(app=app, db=db, User=User)
 
 if __name__ == '__main__':
 	manager.run()
