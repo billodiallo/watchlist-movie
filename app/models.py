e6e6115cd26f09ada2d146174cc7002c2500d02c
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-29 15:42:43 +0300

    login manager loader

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
 
 
 class Movie:
@@ -67,3 +70,16 @@ class Review:
 
         def verify_password(self,password):
             return check_password_hash(self.pass_secure,password)
+            
+            @login_manager.user_loader
+        def load_user(user_id):
+            return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
